c7466e0160e6fe8a7f15ef0ff8ab12e30db384ee
chanakaphd <sajanishyama@gmail.com>
2026-06-05 15:36:15 +0530

    Update reception.js

--- client/reception.js
+++ client/reception.js
@@ -1,1 +1,29 @@
+const saveBtn =
+    document.getElementById("saveBtn");
 
+saveBtn.addEventListener(
+    "click",
+    () => {
+
+        const guestName =
+            document.getElementById(
+                "guestName"
+            ).value;
+
+        const message =
+            document.getElementById(
+                "message"
+            );
+
+        if (!guestName) {
+
+            message.textContent =
+                "Please enter guest name.";
+
+            return;
+        }
+
+        message.textContent =
+            `Guest Saved: ${guestName}`;
+    }
+);
